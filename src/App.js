55ee60465426c4c5586f4895988dfda9ac9ee2d9
Shourya Raj Sharma <0shouryasharma0@gmail.com>
2020-10-03 21:00:50 +0100

    all tasks completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './App.css';
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
+import Error from './component/ErrorPage';
 
 class App extends Component {
   render () {
@@ -32,6 +33,8 @@ class App extends Component {
         <Switch>
           <Route path="/users" component={Users} />
           <Route path="/courses" component={Courses} />
+          <Redirect from="/all-courses" to="/courses" />
+          <Route component={Error} />
         </Switch>
       </div>
     );
---
+++ src/component/ErrorPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const error = () => {
+    return(
+        <h1 className="tracking-in-contract-bck-bottom">Error 404. Page not found</h1>
+    );
+}
+
+export default error;
